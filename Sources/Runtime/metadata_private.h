2c02a84dfad5a1942837115b613562d1040c5298
Harlan Haskins <harlan@harlanhaskins.com>
2017-03-06 18:00:18 -0500

    Added ARC primitives and removed TinyGC files.

--- Sources/Runtime/metadata_private.h
+++ Sources/Runtime/metadata_private.h
@@ -14,24 +14,24 @@
 struct TypeMetadata;
 
 struct FieldMetadata {
-    const char *name;
-    const TypeMetadata *typeMetadata;
-    const size_t offset;
+  const char *name;
+  const TypeMetadata *typeMetadata;
+  const size_t offset;
 };
 
 struct TypeMetadata {
-    const char *name;
-    const FieldMetadata *fields;
-    uint8_t isReferenceType;
-    uint64_t sizeInBits;
-    uint64_t fieldCount;
-    uint64_t pointerLevel;
+  const char *name;
+  const FieldMetadata *fields;
+  uint8_t isReferenceType;
+  uint64_t sizeInBits;
+  uint64_t fieldCount;
+  uint64_t pointerLevel;
 };
 
 struct ProtocolMetadata {
-    const char *name;
-    const char **methodNames;
-    const size_t methodCount;
+  const char *name;
+  const char **methodNames;
+  const size_t methodCount;
 };
 
 struct AnyBox {
@@ -39,8 +39,8 @@ struct AnyBox {
 };
 
 struct GenericBox {
-    const TypeMetadata *typeMetadata;
-    const void **witnessTable;
+  const TypeMetadata *typeMetadata;
+  const void **witnessTable;
 };
 
 #endif /* metadata_private_h */
